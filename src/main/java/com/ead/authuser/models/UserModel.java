3eee1e84212a1da7b0a7966bce64a82b57868242
titiscms <titisbackup@gmail.com>
2022-01-05 23:59:58 -0300

    Correction imports

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.ToString;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
