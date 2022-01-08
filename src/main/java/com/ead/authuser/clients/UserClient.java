759671c6c0b461f198f629a2fb532fa253e5b213
titiscms <titisbackup@gmail.com>
2022-01-07 23:45:54 -0300

    Implementing composition pattern

---
+++ src/main/java/com/ead/authuser/clients/UserClient.java
@@ -0,0 +1,50 @@
+package com.ead.authuser.clients;
+
+import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Log4j2
+@Component
+public class UserClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private UtilsService utilsService;
+
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
+        List<CourseDto> searchResult = new ArrayList<>();
+        String url = utilsService.getUrl(userId, pageable);
+
+        log.debug("Request URL: {} ", url);
+        log.info("Request URL: {} ", url);
+        try {
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<>() {};
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+            log.debug("Response number of elements: {} ", searchResult.size());
+        } catch (HttpStatusCodeException e) {
+            log.error("Error request /courses ", e);
+        }
+        log.info("Ending request /courses userId {}", userId);
+        return new PageImpl<>(searchResult);
+    }
+}
---
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -0,0 +1,38 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.clients.UserClient;
+import com.ead.authuser.dtos.CourseDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping(path = "/users/{userId}/courses")
+public class UserCourseController {
+
+    @Autowired
+    private UserClient userClient;
+
+    @GetMapping
+    public ResponseEntity<Page<CourseDto>> getAllCoursesByUser(@PathVariable UUID userId,
+                                                               @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable) {
+
+        Page<CourseDto> allCoursesByUserPage = userClient.getAllCoursesByUser(userId, pageable);
+//        if (!allCoursesByUserPage.isEmpty()) {
+//            for (CourseDto courseDto : allCoursesByUserPage.toList()) {
+//                courseDto.add(linkTo(methodOn(UserCourseController.class).getAllCoursesByUser(userId, pageable))).withSelRel());
+//            }
+//        }
+        return ResponseEntity.status(HttpStatus.OK).body(allCoursesByUserPage);
+    }
+}
