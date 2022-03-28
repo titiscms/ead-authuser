5ecf89d446912587e96311516f6d85184856f3fe
titiscms <titisbackup@gmail.com>
2022-03-28 20:29:20 -0300

    Basic authentication implementation and creation of role model and its repository and service

---
+++ src/main/java/com/ead/authuser/models/RoleModel.java
@@ -0,0 +1,35 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.RoleType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TB_ROLES")
+public class RoleModel implements GrantedAuthority, Serializable  {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID roleId;
+    @Column(nullable = false, unique = true, length = 30)
+    @Enumerated(EnumType.STRING)
+    private RoleType roleName;
+
+    @Override
+    @JsonIgnore
+    public String getAuthority() {
+        return this.roleName.toString();
+    }
+}
