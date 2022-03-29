5aa49d216e904f9a56fbce338da20911233b588e
titiscms <titisbackup@gmail.com>
2022-03-28 23:10:40 -0300

    Implementing UserDetails for basic auth with database

---
+++ src/main/java/com/ead/authuser/configs/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ead.authuser.configs.security;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException(String.format("Error: User not found with username %s", username)));
+        return UserDetailsImpl.build(userModel);
+    }
+}
