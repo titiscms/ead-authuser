64d5b2b9d00a738c1ebdc46f63f04d697d77812c
titiscms <titisbackup@gmail.com>
2022-04-12 22:02:56 -0300

    Creating new controller and implementing roles authorization

--- src/main/java/com/ead/authuser/controllers/AdminController.java
+++ src/main/java/com/ead/authuser/controllers/AdminController.java
@@ -1,6 +1,6 @@
 package com.ead.authuser.controllers;
 
-import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.dtos.AdminDto;
 import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.RoleModel;
@@ -22,8 +22,8 @@ import java.util.Optional;
 @Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping(path = "/instructors")
-public class InstructorController {
+@RequestMapping(path = "/administrators")
+public class AdminController {
 
     @Autowired
     private UserService userService;
@@ -33,17 +33,18 @@ public class InstructorController {
 
     @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping(path = "/subscription")
-    public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
-        Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
+    public ResponseEntity<Object> saveSubscriptionAdmin(@RequestBody @Valid AdminDto adminDto) {
+        Optional<UserModel> userModelOptional = userService.findById(adminDto.getUserId());
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }
         var userModel = userModelOptional.get();
-        userModel.setUserType(UserType.INSTRUCTOR);
+        userModel.setUserType(UserType.ADMIN);
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
-        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR).orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
+        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_ADMIN).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
         userModel.getRoles().add(roleModel);
         var userModelSaved = userService.updateUser(userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userModelSaved);
+        return ResponseEntity.status(HttpStatus.OK).body(userModelSaved);
     }
+
 }
